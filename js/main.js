2290e382c1a80f0dc30bffa269971f4f463b9c62
MarVikt <v.m.podporogie@gmail.com>
2022-02-03 17:52:00 +0300

    fixed modal's scrolling

--- js/main.js
+++ js/main.js
@@ -1,60 +1,5 @@
 $(document).ready(function() {
 
-  // var hotelSlider = new Swiper('.hotel__photos', {
-  //   // Optional parameters
-  //   direction: 'horizontal',
-  //   loop: true,
-  
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.hotel__photo-button--next',
-  //     prevEl: '.hotel__photo-button--previous',
-  //   },
-  
-  //   keyboard: {
-  //     enabled: true,
-  //     onlyInViewport: true,
-  //   },
-    
-  // });
-  
-  // var aboutUsSlider = new Swiper('.about-us__feedback', {
-  //   // Optional parameters
-  //   direction: 'horizontal',
-  //   loop: true,
-  
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.about-us__button--next',
-  //     prevEl: '.about-us__button--previous',
-  //   },
-  
-  //   keyboard: {
-  //     enabled: true,
-  //     onlyInViewport: true,
-  //   },
-    
-  // });
-  
-  // parallax
-  // document.addEventListener('scroll', event => {
-  //   const
-  //     newsLetter = document.querySelector('.newsletter'),
-  //     { height,  top } = newsLetter.getBoundingClientRect(),
-  //     step = (top + height) / (innerHeight + height),
-  //     // относительная скорость перемещения фона в процентах (от 0 до 100);
-  //     slowing = 60,
-  //     // смещение положения положения фона в процентах 
-  //     shift = 20,
-  //     //     реверс -1 движение фона в противоположную сторону
-  //     reverse = true;
-      
-      
-  //   if ((top - innerHeight <= 0) && (top + height >= 0))
-  //       newsLetter.style.backgroundPositionY =
-  //         `${(100 - (slowing + shift) * (reverse ? -1 : 1)) % 100 + (step * slowing * (reverse ? -1 : 1))}%`;
-  // });
-  
   var menuButton = document.querySelector(".menu-mobile");
     menuButton.addEventListener('click', function() {
     console.log("клик по кнопке мобильного меню");
@@ -70,8 +15,11 @@ $(document).ready(function() {
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    // var modalBody = $('"body"');
     modalOverlay.addClass('modal__overlay_visible');
     modalDialog.addClass('modal__dialog_visible');
+    // modalBody.addClass('body_hidden');
+    document.body.style.overflow = "hidden";
   };
 
   closeModalButton.on('click', closeModal);
@@ -79,17 +27,23 @@ $(document).ready(function() {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    // var modalBody = $('"body"');
     modalOverlay.removeClass('modal__overlay_visible');
     modalDialog.removeClass('modal__dialog_visible');
+    // modalBody.removeClass('body_hidden');
+    document.body.style.overflow = "auto";
   };
 
   $(document).on('keydown',function(event) {
     if (event.keyCode == 27) {
       var modalOverlay = $(".modal__overlay");
       var modalDialog = $(".modal__dialog");
+      // var modalBody = $('"body"');
       modalOverlay.removeClass('modal__overlay_visible');
       modalDialog.removeClass('modal__dialog_visible');
-       }
+      // modalBody.removeClass('body_hidden');
+      document.body.style.overflow = "auto";
+    }
   });
 
 // валидация форм
